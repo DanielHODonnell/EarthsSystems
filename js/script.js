c50fd0ea66ad8abd79709be02d4ce5d06c3fb27f
DanielHODonnell <danielodonnell@unomaha.edu>
2025-04-21 12:21:27 -0500

    Added script.js and style.css

--- js/script.js
+++ js/script.js
@@ -2,6 +2,19 @@ const sun = document.getElementById('sun');
 const earth = document.getElementById('earth');
 
 sun.addEventListener('click', function () {
-    sun.classList.toggle('centered');
-    earth.classList.toggle('offscreen');
+    const isCentered = sun.classList.contains('centered');
+
+    sun.classList.add('animating');
+
+    if (isCentered) {
+        sun.classList.remove('centered');
+        earth.classList.remove('offscreen');
+    } else {
+        sun.classList.add('centered');
+        earth.classList.add('offscreen');
+    }
+
+    setTimeout(() => {
+        sun.classList.remove('animating');
+    }, 1000);
 });
